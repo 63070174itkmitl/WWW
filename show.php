b349bd43aa519ba2fd35ba90e11cd43e70195d04
DESKTOP-TDHTH0H\ing11 <ingUniver>
2020-11-20 23:47:20 +0700

    I'm sorry. I'm stupid more than to make it correct. T T

--- show.php
+++ show.php
@@ -19,6 +19,8 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
     <th width="150"> <div align="center">Action </div></th>
+    <td align="center"><?php echo $row["name"]; ?></td>
+    <td align="center"><?php echo $row["age"]; ?></td>
   </tr>
 </table>
 <?php
