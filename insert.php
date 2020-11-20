5f15df7cbb0c0c5bacf4d32c77eb45df1b67dccb
DESKTOP-TDHTH0H\ing11 <ingUniver>
2020-11-20 23:31:40 +0700

    eieei

--- insert.php
+++ insert.php
@@ -1,19 +1,18 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'ing63070174.mysql.database.azure.com', 'yahogoo00@ing63070174', 'Ing63070174', 'itflab', 3306);
+mysqli_real_connect($conn, 'ing63070174.mysql.database.azure.com', 'yahogoo00@ing63070174', 'Ing63070174', "nowit'sgonnawork", 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 
-$name = $_POST['Name'];
-$comment = $_POST['Comment'];
-$link = $_POST['Action'];
+$name = $_POST['name'];
+$comment = $_POST['comment'];
 
 
-$sql = "INSERT INTO Guestbook (Name , Comment , Action) VALUES ('$name', '$comment', '$action')";
+$sql = "INSERT INTO Guestbook (Name , Comment) VALUES ('$name', '$comment')";
 
 
 if (mysqli_query($conn, $sql)) {
@@ -22,5 +21,5 @@ if (mysqli_query($conn, $sql)) {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
   
-mysqli_close($conn);
+mysqli_close($conn); 
 ?>
